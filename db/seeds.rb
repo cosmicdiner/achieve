f084b0a382b0d15ae558d81882db21f7f478915b
Tomoaki Yano <t.y.village@gmail.com>
2016-07-25 15:47:46 +0000

    シードデータ投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times do |n|
+  name = Faker::Name.name
+  email = Faker::Internet.email
+  password = "password"
+
+  User.create!( name: name,
+                email: email,
+                password: password,
+                password_confirmation: password,
+                )
+end
+
+100.times do |n|
+  content = Faker::Lorem.sentence
+  title = Faker::Name.title
+  Blog.create!( title: title,
+                content: content,
+                user_id: n + 1,
+              )
+end
